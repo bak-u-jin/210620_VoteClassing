c7a637d8bcab35ee46f66a2a8683db8804c0195e
bak-u-jin <73884725+bak-u-jin@users.noreply.github.com>
2021-06-24 17:28:13 +0900

    옵션 무기입시 기입인식 버그 수정

--- Components/Create/OptionList.js
+++ Components/Create/OptionList.js
@@ -33,7 +33,9 @@ function OptionList({ SetOptions }) {
         <TextInput
           key={index}
           onChangeText={(e) => (optionText = e)}
-          onEndEditing={() => SetOptions({ index : index-1, optionText })}
+          onEndEditing={() => {
+            if (optionText) SetOptions({ index: index - 1, optionText });
+          }}
           placeholder={`투표항목 ${index}`}
         />
       ))
